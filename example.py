96506b7bed93cef5758d6fc0a0deff5f3a5d48ea
ot0m1 <coding_addiction@yahoo.co.jp>
2025-07-21 22:19:25 +0900

    add files

---
+++ example.py
@@ -0,0 +1,19 @@
+print('hello world')
+
+def greet():
+    print('こんにちは')
+
+def print_name(name):
+    print(f'私の名前は{name}です')
+
+def get_greet():
+    return 'おはようございます'
+
+def add(a, b):
+    return a + b
+
+greet()
+print_name('Bob')
+print(get_greet())
+print(add(77, 11))
+
